016548d8d2a2479432a869b634653c2563d57543
jamesdev500 <99420333+jamesdev500@users.noreply.github.com>
2022-02-28 15:58:36 +0800

    Update file-input-list-files.js

--- file-input-list-files.js
+++ file-input-list-files.js
@@ -1,10 +1,17 @@
     // Show added files on Job Application Form /careers/job-application-form/
     const filesField = $('.elementor-widget-form.elementor-element-1f7abcb form #form-field-attachments');
-    let fileList = '<br/>';
+    let labelisAltered = false;
+    let fileList;
     $(filesField).change(function() {
-        console.log('typeof: ' + typeof this.files);
-        for (var i = 0; i < this.files.length; i++) {
-            fileList += '<span>' + this.files[i].name + '</span><br/>';
+        if (this.files.length) {
+            for (var i = 0; i < this.files.length; i++) {
+                fileList += '<span>' + this.files[i].name + '</span><br/>';
+            }
+            labelisAltered = true;
+        } else {
+            $(this).prev('label').html('Attachments');
         }
-        $(this).prev('label').append(fileList);
+        if (!labelisAltered) {
+            $(this).prev('label').html('Attachments: <br/>' + fileList);
+        }
     });
