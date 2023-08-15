de9ba321a8955cdf7ab2a3cf3307674881da89ab
jdev <jlacandula@gmail.com>
2023-08-16 05:58:35 +0800

    more scripts

--- file-input-list-files.js
+++ file-input-list-files.js
@@ -1,17 +1,17 @@
-    // Show added files on Job Application Form /careers/job-application-form/
-    const filesField = $('.elementor-widget-form.elementor-element-1f7abcb form #form-field-attachments');
-    let labelisAltered = false;
-    let fileList;
-    $(filesField).change(function() {
-        if (this.files.length) {
-            for (var i = 0; i < this.files.length; i++) {
-                fileList += '<span>' + this.files[i].name + '</span><br/>';
-            }
-            labelisAltered = true;
-        } else {
-            $(this).prev('label').html('Attachments');
+// Show added files on Job Application Form /careers/job-application-form/
+const filesField = $('.elementor-widget-form.elementor-element-1f7abcb form #form-field-attachments');
+let labelisAltered = false;
+let fileList;
+$(filesField).change(function () {
+    if (this.files.length) {
+        for (var i = 0; i < this.files.length; i++) {
+            fileList += '<span>' + this.files[i].name + '</span><br/>';
         }
-        if (!labelisAltered) {
-            $(this).prev('label').html('Attachments: <br/>' + fileList);
-        }
-    });
+        labelisAltered = true;
+    } else {
+        $(this).prev('label').html('Attachments');
+    }
+    if (!labelisAltered) {
+        $(this).prev('label').html('Attachments: <br/>' + fileList);
+    }
+});
